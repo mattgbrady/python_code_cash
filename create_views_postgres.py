b4f59ac893793a3fed81ee9b46e92bbb4887ebf2
Matt Brady <mattgbrady@gmail.com>
2015-06-10 07:53:45 -0700

    create usd_cf view

--- create_views_postgres.py
+++ create_views_postgres.py
@@ -112,7 +112,7 @@ def sti_cf_view(conn,db_name):
 
     upload_to_db(db_name,columns,temp_df, conn)
   
-def operations_cf_view(conn,db_name):
+def cad_operations_cf_view(conn,db_name):
 
     criteria_1 = 'Ending CAD Cash'
     criteria_1 = "'"+criteria_1+"'"
@@ -174,19 +174,79 @@ def operations_cf_view(conn,db_name):
     
 
 
-    temp_df['flag_below_zero'] = np.nan
+    temp_df['flag_below_zero'] = None
 
     temp_df['flag_below_zero'][(temp_df['cash_flow_name'] == 'CAD Operating Balance') & (temp_df['cash_flow_amount'] < 0)] = temp_df['cash_flow_amount'][(temp_df['cash_flow_name'] == 'CAD Operating Balance') & (temp_df['cash_flow_amount'] < 0)].values
 
+    upload_to_db(db_name,columns,temp_df, conn)
+
+def usd_operations_cf_view(conn,db_name):
+
+    criteria_1 = 'Ending USD Cash'
+    criteria_1 = "'"+criteria_1+"'"
+    criteria_2 = 'Ending USD Cash'
+    criteria_2 = "'"+criteria_2+"'"
+    criteria_3 = 'Total USD Inflow'
+    criteria_3 = "'"+criteria_3+"'"
+    criteria_4 = 'Total USD Outflow'
+    criteria_4 = "'"+criteria_4+"'"
+
+    sql = ("SELECT max(created_at) FROM %s WHERE cash_flow_name = %s OR cash_flow_name = %s OR cash_flow_name = %s OR cash_flow_name = %s" % 
+        (db_name,criteria_1,criteria_2,criteria_3, criteria_4))
+
+    conn.execute(sql)
+    latest_created_at = conn.fetchone()
+    latest_created_at = latest_created_at[0]
+    latest_created_at = latest_created_at.strftime("%Y-%m-%d")
+    latest_created_at = "'"+latest_created_at+"'"
+
+    sql =  ("SELECT the_date, cash_flow_name, cash_flow_amount, created_at FROM %s WHERE (cash_flow_name = %s OR cash_flow_name = %s OR cash_flow_name =  %s OR cash_flow_name =  %s) and created_at = %s " % 
+                (db_name,criteria_1, criteria_2, criteria_3, criteria_4, latest_created_at))
+ 
+    conn.execute(sql)
+    
+    data_tuple =  conn.fetchall()
+
+    date_index = [i[0] for i in data_tuple]
+
+    columns= ['the_date','cash_flow_name','cash_flow_amount', 'db_created_at']
+
+    temp_df = df.from_records(data_tuple, columns=columns)
+
+    temp_df = temp_df.sort('the_date')
+
+    current_date = datetime.today()
+    
+    current_date = current_date.now().date()
+  
+    end_date = current_date + timedelta(days=60)
+
+    temp_df = temp_df[(temp_df.the_date >= current_date) & (temp_df.the_date <= end_date)]
 
     
-    for index in temp_df.index:
-        print temp_df.loc[index]
-    #temp_df['flag_below_zero'].replace(str('nan'),None, inplace=True)
-  
-    #print temp_df
-    #upload_to_db(db_name,columns,temp_df, conn)
+    sql = ('TRUNCATE the_zoo.operating_usd_cf')
 
+    db_name = 'the_zoo.operating_usd_cf'
+
+    conn.execute(sql)
+    conn.connection.commit()
+
+    temp_df['number_of_days'] = temp_df['the_date'] - current_date
+    temp_df['number_of_days'] = temp_df['number_of_days'].astype('timedelta64[D]').astype(int)
+
+    temp_df = temp_df.replace(to_replace='Total USD Outflow',value='Outflow')
+    temp_df = temp_df.replace(to_replace='Total USD Inflow',value='Inflow')
+    temp_df = temp_df.replace(to_replace='Ending USD Cash',value='USD Operating Balance')
+
+    columns= ['the_date','cash_flow_name','cash_flow_amount','db_created_at','number_of_days','flag_below_zero']
+    
+
+
+    temp_df['flag_below_zero'] = None
+
+    temp_df['flag_below_zero'][(temp_df['cash_flow_name'] == 'USD Operating Balance') & (temp_df['cash_flow_amount'] < 0)] = temp_df['cash_flow_amount'][(temp_df['cash_flow_name'] == 'USD Operating Balance') & (temp_df['cash_flow_amount'] < 0)].values
+
+    upload_to_db(db_name,columns,temp_df, conn)
 
 
 def main():
@@ -204,8 +264,9 @@ def main():
 
     conn = connect_to_database(host_name,port,username,password,database)
 
-    #sti_cf_view(conn,db_name='the_zoo.sti_cash_flows')
-    operations_cf_view(conn,db_name='the_zoo.sti_cash_flows')
+    sti_cf_view(conn,db_name='the_zoo.sti_cash_flows')
+    cad_operations_cf_view(conn,db_name='the_zoo.sti_cash_flows')
+    usd_operations_cf_view(conn,db_name='the_zoo.sti_cash_flows')
 
 
     end_time = time.time()
