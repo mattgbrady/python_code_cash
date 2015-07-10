a9f29f9e785161ff288b6417484261b421967425
Matt Brady <mattgbrady@gmail.com>
2015-07-10 15:16:32 -0700

    create maturity bucket view. still need to add zeroes to all buckets

--- create_portfolio_views_postgres.py
+++ create_portfolio_views_postgres.py
@@ -126,7 +126,7 @@ def check_security_duplicates(ticker_temp_df):
         print ticker_temp_df[ticker_temp_df.security_bloomberg_ticker_dup == True]
         sys.exit("ERROR");
 
-def join_data_frames(daily_mv_df,ticker_information_df,issuer_df):
+def join_type_view_df(daily_mv_df,ticker_information_df,issuer_df):
 
     ticker_temp_df = ticker_information_df[['security_bloomberg_ticker','issuer_name','annualized_yield','maturity_date','category']]
     check_security_duplicates(ticker_temp_df)
@@ -147,7 +147,7 @@ def join_data_frames(daily_mv_df,ticker_information_df,issuer_df):
 
     return daily_mv_df
 
-def create_existing_sub_group_view(conn,daily_mv_df,table_name,group_type):
+def type_view(conn,daily_mv_df,table_name,group_type):
 
     grouped = daily_mv_df.groupby(by=['the_date',group_type]).sum()
 
@@ -159,17 +159,63 @@ def create_existing_sub_group_view(conn,daily_mv_df,table_name,group_type):
 
     upload_to_db(conn,temp_df,table_name)
 
-def maturity_schedule_view(conn,daily_mv_df,table_name):
+def maturity_bucket(row):
+    
+    if row.number_of_days == 1:
+        return 'Overnight'
+    elif row.number_of_days > 1 and row.number_of_days <= 5:
+        return '2-5'
+    elif row.number_of_days > 5 and row.number_of_days <= 10:
+        return '6-10'   
+    elif row.number_of_days > 10 and row.number_of_days <= 20:
+        return '11-20'  
+    elif row.number_of_days > 20 and row.number_of_days <= 30:
+        return '21-30'  
+    elif row.number_of_days > 30 and row.number_of_days <= 45:
+        return '31-45'  
+    elif row.number_of_days > 45 and row.number_of_days <= 60:
+        return '46-60'  
+    elif row.number_of_days > 60 and row.number_of_days <= 90:
+        return '61-90'  
+    elif row.number_of_days > 90:
+        return '>91'  
 
-    return
+def maturity_bucket_view(conn,daily_mv_df,ticker_information_df,table_name):
 
-    #grouped = daily_mv_df.groupby(by=['the_date',group_type]).sum()
+    #only to be used for testing this code
+    #date should be current
+    testing_date = min(daily_mv_df['the_date']).date()
 
-    #temp_df = df(grouped)
+    #ticker_information_df['number_of_days'] = daily_mv_df['the_date'] - testing_date
 
-    #temp_df.reset_index(inplace=True)
+    daily_mv_df['number_of_days'] = daily_mv_df['maturity_date'] - testing_date
 
-    #temp_df = temp_df[['the_date',group_type,'percentage']]
+
+    daily_mv_df['number_of_days'] = daily_mv_df['number_of_days'].astype('timedelta64[D]').astype(int)
+
+    daily_mv_df.number_of_days[daily_mv_df['security_bloomberg_ticker'] == 'Cash'] = 1
+
+    daily_mv_df['maturity_bucket'] = None
+
+    daily_mv_df['maturity_bucket'] = daily_mv_df.apply(maturity_bucket, axis=1)
+
+    grouped = daily_mv_df.groupby(by=['the_date','maturity_bucket']).sum()
+
+    temp_df = df(grouped)
+
+    
+    temp_df.reset_index(inplace=True)
+    unique_date = set(temp_df['the_date'].values)
+    unique_date = unique_date[x.date for x in unique_date]
+
+    bucket_array = ['Overnight', '2-5', '6-10','11-20','21-30','31-45','46-60','61-90','>91']
+
+
+
+    temp_df.reset_index(inplace=True)
+
+    temp_df = temp_df[['the_date','maturity_bucket','market_value','percentage']]
+
 
     #upload_to_db(conn,temp_df,table_name)
 
@@ -181,7 +227,7 @@ def process_daily_data(conn):
     
     daily_mv_df = get_positions_percentage(daily_mv_df,index=['the_date','security_bloomberg_ticker'])
 
-    daily_mv_df = join_data_frames(daily_mv_df,ticker_information_df,issuer_df)
+    daily_mv_df = join_type_view_df(daily_mv_df,ticker_information_df,issuer_df)
 
     daily_mv_df.drop('id', axis=1, inplace=True)
 
@@ -203,10 +249,10 @@ def main():
 
     daily_mv_df, ticker_information_df = process_daily_data(conn)
 
-    create_existing_sub_group_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_sector_view',group_type='security_sector')
-    create_existing_sub_group_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_industry_view',group_type='industry')
-    create_existing_sub_group_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_instrument_view',group_type='category')
-    #maturity_schedule_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_maturity_bucket_view')
+    #type_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_sector_view',group_type='security_sector')
+    #type_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_industry_view',group_type='industry')
+    #type_view(conn,daily_mv_df, table_name = 'the_zoo.sti_daily_instrument_view',group_type='category')
+    maturity_bucket_view(conn,daily_mv_df,ticker_information_df, table_name = 'the_zoo.sti_daily_maturity_bucket_view')
 
 
     end_time = time.time()
